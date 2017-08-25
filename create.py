360690a6d5a42a059110e7cb6a89c09922672f91
Felix Breidenstein <mail@felixbreidenstein.de>
2017-08-25 22:27:40 +0200

    Added some commandlines arguments and possibility to use multiple templates

--- create.py
+++ create.py
@@ -1,29 +1,47 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import click
+import os
+import qrcode
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
-from datetime import datetime
-import qrcode
 
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template("template.html")
 
-# generate qr-code
-qr = qrcode.QRCode(
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=3
-    )
-qr.add_data('https://wiki.darmstadt.ccc.de/Projekte/3D-Drucker')
-img = qr.make_image()
-img.save("qrcode.png")
+@click.command()
+@click.option('--template', '-t', help="Define the filename of the template to use")
+@click.option('--owner', '-o', help="Write the owner onto the label")
+@click.argument("name")
+@click.argument("url")
+def generator(template, owner, name, url):
 
-# Fill variables
-template_vars = {"title": "3D-Drucker", "owner": "fleaz"}
+    if not template:
+        template = "default.html"
 
-# Render template
-html_out = template.render(template_vars)
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template(template)
 
-# Generate pdf
-HTML(string=html_out, base_url="/home/fleaz/workspace/python/qrcode-labels").write_pdf("label.pdf")
+    # generate qr-code
+    qr = qrcode.QRCode(
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=3)
+
+    qr.add_data(url)
+    img = qr.make_image()
+    img.save("qrcode.png")
+
+    # Fill variables
+    template_vars = {"name": name}
+    if owner:
+        template_vars["owner"] = owner
+
+    # Render HTML template with variables
+    html_out = template.render(template_vars)
+
+    # Generate pdf
+    HTML(string=html_out, base_url=os.path.realpath(__file__)).write_pdf("label.pdf")
+
+if __name__ == "__main__":
+    generator()
 
